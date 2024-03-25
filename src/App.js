c1ac0883b955f0980ca02259ff791664510d3dfc
Aoife Kirby <aoifekirby33@gmail.com>
2024-03-25 22:45:39 +0000

    Add navbar and logo, set up routes for nav links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import NavBar from './components/NavBar';
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Placeholder</h1>
+    <div className={styles.App}>
+      <NavBar />
+      <Switch>
+        <Route exact path='/' render={() => <h1>Home Page</h1>}></Route>
+        <Route exact path='/log-in' render={() => <h1>Log In</h1>}></Route>
+        <Route exact path='/sign-up' render={() => <h1>Sign Up</h1>}></Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import logo from '../assets/tasked-logo.png';
+import styles from '../styles/NavBar.module.css';
+import { NavLink } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <Navbar 
+    collapseOnSelect 
+    expand="md" 
+    className={styles.NavBar}
+    >
+      <Container>
+        <NavLink
+        exact 
+        to='/'
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        >
+      <Navbar.Brand>
+        <img
+          src={logo}
+          width="70"
+          height="70"
+          className="d-inline-block align-top"
+          alt="Tasked logo"
+        />
+      </Navbar.Brand>
+      </NavLink>
+      </Container>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+      <Container>
+      <Nav>
+        <NavLink
+         to='log-in'
+         className={styles.NavLink}
+         activeClassName={styles.Active}
+         >
+        <i class="fa-solid fa-right-to-bracket"></i> Log In
+          </NavLink>
+        <NavLink 
+        to='sign-up'
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        >
+        <i class="fa-solid fa-user-plus"></i> Sign Up
+        </NavLink>
+      </Nav>
+      </Container>
+    </Navbar.Collapse>
+  </Navbar>
+  )
+}
+
+export default NavBar;
