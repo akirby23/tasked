4aaa760b85473492a13fbd33d5e7d0a612d384b5
Aoife Kirby <aoifekirby33@gmail.com>
2024-03-26 20:31:05 +0000

    Add and configure axiosReq & axiosRes interceptors

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,11 @@
 import axios from "axios";
 
+// Base defaults
 axios.defaults.baseURL = 'https://drf-tasked-ec51bc3bfc2d.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
+
+// Intercepts the request
+export const axiosReq = axios.create();
+// Intercepts the response
+export const axiosRes = axios.create();
--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useContext, useEffect, useMemo, useState } from 'react';
+import { axiosReq, axiosRes } from '../api/axiosDefaults';
 
 // Code obtained from CI's Moments walkthrough project
 export const CurrentUserContext = createContext();
@@ -10,10 +11,11 @@ export const useSetCurrentUser = () => useContext(SetCurrentUserContext);
 
 export const CurrentUserProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null)
+    const history = useHistory();
 
     const handleMount = async () => {
         try {
-            const { data } = await axios.get('dj-rest-auth/user/')
+            const { data } = await axiosRes.get('dj-rest-auth/user/')
             setCurrentUser(data)
         } catch (err) {
             console.log(err)
@@ -23,6 +25,50 @@ export const CurrentUserProvider = ({ children }) => {
     useEffect(() => {
         handleMount()
     }, []);
+
+    useMemo(() => {
+        // Refreshes access token before making a request
+        axiosReq.interceptors.request.use(
+            async (config) => {
+                try {
+                    await axios.post('/dj-rest-auth/token/refresh/');
+                } catch(err) {
+                    setCurrentUser((prevCurrentUser) => {
+                        if (prevCurrentUser) {
+                            history.push('/log-in');
+                        }
+                        return null;
+                    })
+                    return config;
+                }
+                return config;
+            },
+            (err) => {
+                return Promise.reject(err);
+            }
+        )
+
+        // Refresh access token if a 401 error is thrown
+        axiosRes.interceptors.response.use(
+            (response) => response,
+            async (err) => {
+                if (err.response?.status === 401) {
+                    try {
+                        await axios.post('/dj-rest-auth/token/refresh/')
+                    } catch (err) {
+                        setCurrentUser(prevCurrentUser => {
+                            if (prevCurrentUser){
+                                history.push('/log-in')
+                            }
+                            return null
+                        })
+                    }
+                    return axios.Cancel(err.config)
+                }
+                return Promise.reject(err)
+            }
+        )
+    }, [history])
 
     return (
         <CurrentUserContext.Provider value={currentUser} >
