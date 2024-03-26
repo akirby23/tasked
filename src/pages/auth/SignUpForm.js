273349901f2ac8f112fb4a5738f06b545ebbc4f2
Aoife Kirby <aoifekirby33@gmail.com>
2024-03-26 00:14:39 +0000

    Add sign up form component

---
+++ src/pages/auth/SignUpForm.js
@@ -0,0 +1,115 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+import { Row, Col, Form, Button, Container, Alert } from 'react-bootstrap'
+import { Link, useHistory } from 'react-router-dom'
+
+const SignUpForm = () => {
+    const [signUpData, setSignUpData] = useState({
+        username: '',
+        password1: '',
+        password2: '',
+    });
+    const { username, password1, password2 } = signUpData;
+
+    const [errors, setErrors] = useState({})
+
+    const history = useHistory()
+
+    const handleChange = (event) => {
+        setSignUpData({
+            ...signUpData,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post('dj-rest-auth/registration/', signUpData)
+            history.push('/log-in')
+            
+        } catch (err) {
+            setErrors(err.response?.data)
+        }
+    }
+    return (
+        <Row>
+            <Col>
+                <Container>
+                    <h1>Sign Up</h1>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group controlId="username">
+                            <Form.Label className="d-none">Username</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter username"
+                                name="username"
+                                value={username}
+                                onChange={handleChange}
+                            />
+                        </Form.Group>
+                        {errors.username?.map((message, idx) =>
+                        <Alert variant="warning" key={idx}>
+                            {message}
+                        </Alert>
+                        )}
+
+                        <Form.Group controlId="password1">
+                            <Form.Label className="d-none">Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Password"
+                                name="password1"
+                                value={password1}
+                                onChange={handleChange}
+                            />
+                        </Form.Group>
+                        {errors.password1?.map((message, idx) =>
+                        <Alert variant="warning" key={idx}>
+                            {message}
+                        </Alert>
+                        )}
+
+                        <Form.Group controlId="password2">
+                            <Form.Label className="d-none">Confirm Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Confirm password"
+                                name="password2"
+                                value={password2}
+                                onChange={handleChange}
+                            />
+                        </Form.Group>
+                        {errors.password2?.map((message, idx) =>
+                        <Alert variant="warning" key={idx}>
+                            {message}
+                        </Alert>
+                        )}
+                        <Button
+                            variant="primary"
+                            type="submit">
+                            Sign Up Now
+                        </Button>
+                        {errors.non_field_errors?.map((message, idx) =>
+                        <Alert variant="warning" key={idx}>
+                            {message}
+                        </Alert>
+                        )}
+                    </Form>
+                </Container>
+                <Container>
+                    <Link
+                        to="/log-in"
+                    >
+                        Already have an account? <span>Log In</span>
+                    </Link>
+                </Container>
+            </Col>
+            <Col>
+            Placeholder
+            </Col>
+        </Row>
+    )
+}
+
+export default SignUpForm
