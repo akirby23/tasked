562b98fc32ff53590e42dc9e7a81415b8770d14b
Aoife Kirby <aoifekirby33@gmail.com>
2024-03-26 00:09:44 +0000

    Connect drf-tasked API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import { Route, Switch } from 'react-router-dom'
+import axios from './api/axiosDefaults';
+import SignUpForm from './pages/auth/SignUpForm';
 
 function App() {
   return (
@@ -9,7 +11,7 @@ function App() {
       <Switch>
         <Route exact path='/' render={() => <h1>Home Page</h1>}></Route>
         <Route exact path='/log-in' render={() => <h1>Log In</h1>}></Route>
-        <Route exact path='/sign-up' render={() => <h1>Sign Up</h1>}></Route>
+        <Route exact path='/sign-up' render={() => <SignUpForm />}></Route>
       </Switch>
     </div>
   );
