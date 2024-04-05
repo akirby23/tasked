9a792c2d78011b8fc28e21a3fdddf9af8bad4eb9
Aoife Kirby <aoifekirby33@gmail.com>
2024-04-05 00:15:00 +0000

    Add ProfileDataContext

---
+++ src/contexts/ProfileDataContext.js
@@ -0,0 +1,44 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import { axiosReq } from "../api/axiosDefaults";
+import { useCurrentUser } from "./CurrentUserContext";
+
+// Code adapted from CI's Moments walkthrough project
+const ProfileDataContext = createContext();
+const SetProfileDataContext = createContext();
+
+export const useProfileData = () => useContext(ProfileDataContext);
+export const useSetProfileData = () => useContext(SetProfileDataContext);
+
+export const ProfileDataProvider = ({ children }) => {
+  const [profileData, setProfileData] = useState({
+    pageProfile: { results: [] },
+  });
+
+  const currentUser = useCurrentUser();
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          '/profiles/?ordering=-tasks_count'
+        );
+        setProfileData((prevState) => ({
+          ...prevState,
+          profiles: data,
+        }));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [currentUser]);
+
+  return (
+    <ProfileDataContext.Provider value={profileData}>
+      <SetProfileDataContext.Provider value={setProfileData}>
+        {children}
+      </SetProfileDataContext.Provider>
+    </ProfileDataContext.Provider>
+  );
+};
