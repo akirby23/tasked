29128e917cbb414b333df83b1160a573447fb832
Aoife Kirby <aoifekirby33@gmail.com>
2024-04-10 15:15:47 +0000

    Add 404 page & styling

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ import ProfilePage from './pages/profiles/ProfilePage.js';
 import EditUsernameForm from './pages/profiles/EditUsernameForm.js';
 import EditProfileForm from './pages/profiles/EditProfileForm.js';
 import EditPasswordForm from './pages/profiles/EditPasswordForm.js';
+import HomePage from './pages/home/HomePage.js';
+import NotFound from './components/NotFound.js';
 
 
 function App() {
@@ -27,7 +29,7 @@ function App() {
       <NavBar />
       <Container>
         <Switch>
-          <Route exact path='/' render={() => <h1>Home Page</h1>}></Route>
+          <Route exact path='/' render={() => <HomePage />}></Route>
           <Route exact path='/my-tasks' render={() => <TasksPage
           filter={`owner__profile=${profile_id}&`}
           message='No results found. Adjust the search keyword or create a task.' />}></Route>
@@ -42,13 +44,12 @@ function App() {
           <Route exact path='/log-out' render={() => <LogOut />}></Route>
           <Route exact path='/create-task' render={() => <CreateTaskForm />}></Route>
           <Route exact path='/tasks/:id/edit' render={() => <EditTaskForm />}></Route>
-          <Route exact path='/my-assigned-tasks' render={() => <h1>My Assigned Tasks</h1>}></Route>
           <Route exact path='/tasks/:id' render={() => <TaskPage />}></Route>
           <Route exact path='/profiles/:id' render={() => <ProfilePage />}></Route>
           <Route exact path='/profiles/:id/edit' render={() => <EditProfileForm />}></Route>
           <Route exact path="/profiles/:id/edit/username" render={() => <EditUsernameForm />}></Route>
           <Route exact path="/profiles/:id/edit/password" render={() => <EditPasswordForm/>}></Route>
-          <Route render={() => <p>Page not found</p>} />
+          <Route render={() => <NotFound message="Sorry, the page you're looking for could not be found." />} />
         </Switch>
         <Toaster 
         position='bottom-right'
---
+++ src/components/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageNotFound from '../assets/404.png';
+import Asset from './Asset';
+
+const NotFound = ({ message }) => {
+    return (
+            <Asset
+            src={PageNotFound}
+            message={message}
+        />
+    );
+};
+
+export default NotFound
